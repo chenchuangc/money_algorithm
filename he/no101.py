71576eddb02e7c33c42a888c6b24a0042b7d6bd2
heyuanye <1403237206@qq.com>
2020-01-27 17:46:53 +0800

    对称二叉树

---
+++ he/no101.py
@@ -0,0 +1,37 @@
+'''
+对称二叉树
+public boolean isSymmetric(TreeNode root) {
+    return isMirror(root, root);
+}
+
+public boolean isMirror(TreeNode t1, TreeNode t2) {
+    if (t1 == null && t2 == null) return true;
+    if (t1 == null || t2 == null) return false;
+    return (t1.val == t2.val)
+        && isMirror(t1.right, t2.left)
+        && isMirror(t1.left, t2.right);
+}
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/symmetric-tree/solution/dui-cheng-er-cha-shu-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isMirror(self, t1: 'TreeNode' , t2: 'TreeNode') -> bool:
+        if (not t1) and (not t2):return True
+        if (not t1) or (not t2):return False
+        if (t1.val==t2.val) and self.isMirror(t1.left,t2.right) and self.isMirror(t1.right,t2.left):
+            return True
+        else:
+            return False
+
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return self.isMirror(root,root)
