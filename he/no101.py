ab6544cf4da8a448b875ef4580999d8d052bc84b
heyuanye <1403237206@qq.com>
2020-01-27 18:45:58 +0800

    对称二叉树，注释中添加网上Java迭代代码

--- he/no101.py
+++ he/no101.py
@@ -12,6 +12,26 @@ public boolean isMirror(TreeNode t1, TreeNode t2) {
         && isMirror(t1.left, t2.right);
 }
 
+public boolean isSymmetric(TreeNode root) {
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    q.add(root);
+    while (!q.isEmpty()) {
+        TreeNode t1 = q.poll();
+        TreeNode t2 = q.poll();
+        if (t1 == null && t2 == null) continue;
+        if (t1 == null || t2 == null) return false;
+        if (t1.val != t2.val) return false;
+        q.add(t1.left);
+        q.add(t2.right);
+        q.add(t1.right);
+        q.add(t2.left);
+    }
+    return true;
+}
+
+
+
 作者：LeetCode
 链接：https://leetcode-cn.com/problems/symmetric-tree/solution/dui-cheng-er-cha-shu-by-leetcode/
 来源：力扣（LeetCode）
